d6e404eb64ce6978ac3c51e74a086bf671366a7e
Apolon Pachulia <a.pachulia@hotmail.com>
2015-02-27 18:54:35 +0400

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 
 $mc = new Memcached();
-$mc->addServer("127.0.0.1", 11211);
+$mc->addServer("127.0.0.1", 11211); //127.0.0.1 is localhost and 11211 is default port of memcached
  
 $result = $mc->get("any_key");
  
@@ -10,7 +10,7 @@ if($result) {
   echo $result;
 } else {
   echo "No Data";
-  $mc->set("any_key", "Data From Memcached", 1200) or die ("Error");
+  $mc->set("any_key", "Data From Memcached", 1200) or die ("Error"); 
 }
  
  
